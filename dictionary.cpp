40fea36a86627f26e68eb5e5a8596fe948e1a0bc
Bogdan <106272727+Devilnutt@users.noreply.github.com>
2024-02-12 19:35:59 +0300

    dictionary.cpp

--- dictionary.cpp
+++ dictionary.cpp
@@ -5,6 +5,7 @@
 
 class DictionaryList
 {
+public:
     class Node
     {
     public:
@@ -16,9 +17,12 @@ class DictionaryList
 
     Node* _head;
 
-public:
     DictionaryList() : _head(nullptr) {}
 
+    Node* begin() const {
+        return _head;
+    }
+    
     void insert(const std::string& word)
     {
         Node* newNode = new Node(word);
@@ -127,6 +131,7 @@ public:
 
 int main() {
     setlocale(LC_ALL, "Russian");
+    
     DictionaryList dict1, dict2;
 
     dict1.insert("apple");
@@ -137,11 +142,11 @@ int main() {
     dict2.insert("grape");
     dict2.insert("orange");
 
-    std::cout << "Dictionary 1: ";
-    Node* current = dict1._head;
-    while (current != nullptr) {
-        std::cout << current->word << " ";
-        current = current->next;
+    std::cout << "Словарь 1: ";
+    DictionaryList::Node* current1 = dict1.begin(); 
+    while (current1 != nullptr) {
+        std::cout << current1->word << " ";
+        current1 = current1->next;
     }
     std::cout << std::endl;
 
@@ -151,40 +156,40 @@ int main() {
     dict1.remove("banana");
 
     std::cout << "Словарь 1 после удаления слова «banana»: ";
-    current = dict1._head;
-    while (current != nullptr) {
-        std::cout << current->word << " ";
-        current = current->next;
+    DictionaryList::Node* current2 = dict1.begin();
+    while (current2 != nullptr) {
+        std::cout << current2->word << " ";
+        current2 = current2->next;
     }
     std::cout << std::endl;
 
     dict1.merge(dict2);
 
     std::cout << "Словарь 1 после объединения со Словарем 2: ";
-    current = dict1._head;
-    while (current != nullptr) {
-        std::cout << current->word << " ";
-        current = current->next;
+    DictionaryList::Node* current3 = dict1.begin();
+    while (current3 != nullptr) {
+        std::cout << current3->word << " ";
+        current3 = current3->next;
     }
     std::cout << std::endl;
 
     dict1.deleteWords(dict2);
 
     std::cout << "Словарь 1 после удаления слов из в Словаря 2: ";
-    current = dict1._head;
-    while (current != nullptr) {
-        std::cout << current->word << " ";
-        current = current->next;
+    DictionaryList::Node* current4 = dict1.begin();
+    while (current4 != nullptr) {
+        std::cout << current4->word << " ";
+        current4 = current4->next;
     }
     std::cout << std::endl;
 
     DictionaryList intersection = getIntersection(dict1, dict2);
 
     std::cout << "Пересечение Словаря 1 и Словаря 2: ";
-    current = intersection._head;
-    while (current != nullptr) {
-        std::cout << current->word << " ";
-        current = current->next;
+    DictionaryList::Node* current5 = intersection.begin();
+    while (current5 != nullptr) {
+        std::cout << current5->word << " ";
+        current5 = current5->next;
     }
     std::cout << std::endl;
 
